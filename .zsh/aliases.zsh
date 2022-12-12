6c02a7417e5c2659e5bf9fb1ea48cb916c2d7a7e
Ridwan Ikhsan <ridwan.jsx@gmail.com>
2022-12-13 00:21:34 +0700

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,6 +2,3 @@
 alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons --group-directories-first -l"
 alias grep="grep --color"
-
-# show git credential (bitbucket.bri.co.id)
-alias bitbucket-bri="cat /home/cyanogen/Docs/bitbucket-bri.txt"
